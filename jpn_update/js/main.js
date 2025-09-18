35e1ffcdf05d5f2e9569ed5791b70339484c320c
sokomin <sokomincontact@gmail.com>
2025-09-18 21:19:14 +0900

    0.0864

--- jpn_update/js/main.js
+++ jpn_update/js/main.js
@@ -19,7 +19,7 @@ function fetchCSVAndOutput() {
 }
 
 // バージョンアップしたらここ書き換える
-CURRENT_VER=862
+CURRENT_VER=864
 
 for (var i = CURRENT_VER; i >= 746; i--) {
     var value = (i - CURRENT_VER) / 10000 + CURRENT_VER / 10000;
